afbd309252890e65e7eb4735b8566593ae177298
Chandler <chandlermdavidson@gmail.com>
2018-09-18 17:56:28 -0500

    Git Init

---
+++ Project1/Matrix.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Matrix : List<List<double>>
+    {
+        public static bool operator < (Matrix first, Matrix second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator > (Matrix first, Matrix second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Equals (Matrix matrix)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator == (Matrix first, Matrix second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator != (Matrix first, Matrix second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            var str = "";
+            foreach (var row in this)
+            {
+                str += string.Join(" ", row) + "\n";
+            }
+
+            return str;
+        }
+    }
+}
---
+++ Project1/Parser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Parser
+    {
+        public string FilePath { get; set; } = "input.txt";
+
+        public Matrix ParseFile()
+        {
+            var fileLines = File.ReadAllLines(FilePath);
+            var matrix = new Matrix();
+
+            matrix.AddRange(fileLines.Skip(1)
+                .Select(x => x.Split(' ').Select(y => double.Parse(y)).ToList()));
+
+            return matrix;
+        }
+    }
+}
